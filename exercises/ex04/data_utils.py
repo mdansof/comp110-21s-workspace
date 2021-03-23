4e0453711eead125b6096b16d0b68b489cb7ed05
Manuela Danso-Fordjour <manuela7@live.unc.edu>
2021-03-23 00:27:47 -0400

    first functions

--- exercises/ex04/data_utils.py
+++ exercises/ex04/data_utils.py
@@ -1,6 +1,6 @@
 """Utility functions for wrangling data."""
 
-__author__ = "YOUR PID HERE"
+__author__ = "730316345"
 
 
 from csv import DictReader
@@ -11,8 +11,24 @@ def read_csv_rows(csv_file: str) -> list[dict[str, str]]:
     rows: list[dict[str, str]] = []
     
     # TODO 0.1) Complete the implementation of this function here.
-
+    file_handle = open("data/nc_durham_2015_march_21_to_27.csv", "r", encoding= "utf8")
+    csv_reader = DictReader(file_handle)
+    for row in csv_reader:
+        rows.append(row)
+    file_handle.close()
     return rows
 
+def column_values(rows:list[dict[str, str]], title:str) -> list[str]:
+  final_list: list[str] = []
+  for row in rows:
+      final_list.append(str(row[title]))
+   
+   return final_list
 
-# TODO: Define the other functions here.
+def columnar(original:list[dict[str, str]]) ->dict[str, list[str]]:
+    newDict: dict[str,list[str]]= []
+    for row in original:
+        for column in row:
+            newDict[row,column_values]
+        
+    return newDict 
